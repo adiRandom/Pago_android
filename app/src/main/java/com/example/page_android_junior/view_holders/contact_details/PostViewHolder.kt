a72e4562b26e4af95b51f0964131d5aa184d59e4
Adrian Pascu <me.adi.hf@gmail.com>
2022-03-19 12:03:08 +0200

    Done layout for post view holder

---
+++ app/src/main/java/com/example/page_android_junior/view_holders/contact_details/PostViewHolder.kt
@@ -0,0 +1,16 @@
+package com.example.page_android_junior.view_holders.contact_details
+
+import com.example.page_android_junior.databinding.ContactViewHolderBinding
+import com.example.page_android_junior.databinding.PostViewHolderBinding
+import com.example.page_android_junior.models.Post
+import com.example.page_android_junior.models.User
+import com.example.page_android_junior.view_holders.generic.ListViewHolder
+
+class PostViewHolder(val viewBinding: PostViewHolderBinding) :
+    ListViewHolder<Post>(viewBinding) {
+
+    override fun bind(item: Post) {
+        this.viewBinding.post = item
+    }
+
+}
