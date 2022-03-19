764335dc2b8cfa82671abc8a1c6302223a9ebe6d
Adrian Pascu <me.adi.hf@gmail.com>
2022-03-19 12:22:42 +0200

    Configured navigation

--- app/src/main/java/com/example/page_android_junior/models/User.kt
+++ app/src/main/java/com/example/page_android_junior/models/User.kt
@@ -2,13 +2,17 @@ package com.example.page_android_junior.models
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.core.content.res.ResourcesCompat
 import com.example.page_android_junior.R
 import com.example.page_android_junior.models.api.UserApi
 import com.example.page_android_junior.utils.image.generateAvatarForUser
 import com.squareup.picasso.Picasso
+import kotlinx.android.parcel.Parcelize
 
 const val IMAGE_URL = "https://picsum.photos/200/200 "
+
 
 class User(
     id: Int,
@@ -17,8 +21,22 @@ class User(
     gender: String,
     status: String,
 
-    ) : UserApi(id, name, email, gender, status) {
+    ) : UserApi(id, name, email, gender, status), Parcelable {
     lateinit var avatar: Bitmap
+
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+    ) {
+        val bitmap = parcel.readParcelable<Bitmap>(Bitmap::class.java.classLoader);
+        if (bitmap != null) {
+            avatar = bitmap
+        }
+
+    }
 
     constructor(userApi: UserApi) : this(
         userApi.id,
@@ -53,6 +71,29 @@ class User(
             if (image != null) {
                 avatar = image;
             }
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id);
+        parcel.writeString(name)
+        parcel.writeString(email)
+        parcel.writeString(gender)
+        parcel.writeString(status)
+        parcel.writeParcelable(avatar, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
         }
     }
 }
--- app/src/main/java/com/example/page_android_junior/models/api/UserApi.kt
+++ app/src/main/java/com/example/page_android_junior/models/api/UserApi.kt
@@ -2,12 +2,13 @@ package com.example.page_android_junior.models.api
 
 import android.graphics.Bitmap
 import android.media.Image
+import android.os.Parcelable
 import androidx.core.content.res.ResourcesCompat
 import com.example.page_android_junior.R
 import com.example.page_android_junior.services.api.ApiInstance
 import com.example.page_android_junior.utils.image.generateAvatarForUser
 import com.squareup.picasso.Picasso
-
+import kotlinx.parcelize.Parcelize
 
 open class UserApi(
     val id: Int,
@@ -16,5 +17,5 @@ open class UserApi(
     val gender: String,
 //    This could be a Enum if all the known values were known
     val status: String,
-) ;
+);
 
